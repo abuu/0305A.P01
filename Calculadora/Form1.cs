01c918df6c4bc3d81ec53c986ae58544b7ad14d5
abuu <leo_cat_2006@hotmail.com>
2013-09-09 16:43:31 -0500

    Avance

--- Calculadora/Form1.cs
+++ Calculadora/Form1.cs
@@ -11,6 +11,17 @@ namespace Calculadora
 {
     public partial class Form1 : Form
     {
+        private double datos1;/*aqui va las variables globales*/
+        private double datos2;
+        private char operacion;
+        private double rpta;
+        int x = 1;
+        Form1(int datos1, int datos2,int dato3, char operacion)
+        {
+            this.datos1 = datos1;
+            this.datos2 = datos2;
+            this.operacion = operacion;
+        }
         public Form1()
         {
             InitializeComponent();
@@ -19,6 +30,152 @@ namespace Calculadora
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            if (textBox1.Text != "")
+            {
+                if (operacion != Convert.ToChar("+"))//cambia de una operacion a otra
+                {
+                    x = 1;
+                }
+                if (x!=1)
+                {
+                    this.datos2 = Convert.ToInt32(textBox1.Text);
+                    this.operacion = Convert.ToChar("+");
+                    textBox1.Text = Convert.ToString(Toperacion());
+                    datos1 = rpta;
+                    //this.datos1 = datos2;
+                    //this.datos2 = Convert.ToInt32(null);
+                }
+                else
+                {
+                    this.datos1 = Convert.ToInt32(textBox1.Text);
+                    this.operacion = Convert.ToChar("+");
+                    textBox1.Text = "";
+                    x = 2;
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (this.datos1 != null && this.datos2 != null)
+            {
+                this.datos2 = Convert.ToInt32(textBox1.Text);
+                textBox1.Text = "";
+                textBox1.Text = Convert.ToString(Toperacion());
+            }
+        }
+        private double Toperacion()
+        {
+            if (this.operacion == Convert.ToChar("+"))
+            {
+                return rpta=this.datos1 + this.datos2;
+            }
+            else
+            {
+                if (this.operacion == Convert.ToChar("-"))
+                {
+                    return rpta=this.datos1 - this.datos2;
+                }
+                else
+                {
+                    if (this.operacion == Convert.ToChar("*"))
+                    {
+                        return rpta=this.datos1 * this.datos2;
+                    }
+                    else
+                    {
+                        return rpta=this.datos1 / this.datos2;
+                    }
+
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                if (operacion != Convert.ToChar("-"))//cambia de una operacion a otra
+                {
+                    x = 1;
+                }
+                if (x != 1)
+                {
+                    this.datos2 = Convert.ToInt32(textBox1.Text);
+                    this.operacion = Convert.ToChar("-");
+                    textBox1.Text = Convert.ToString(Toperacion());
+                    datos1 = rpta;
+                    //this.datos1 = datos2;
+                    //this.datos2 = Convert.ToInt32(null);
+                }
+                else
+                {
+                    this.datos1 = Convert.ToInt32(textBox1.Text);
+                    this.operacion = Convert.ToChar("-");
+                    textBox1.Text = "";
+                    x = 2;
+                }
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (operacion != Convert.ToChar("*"))//cambia de una operacion a otra
+                {
+                    x = 1;
+                }
+            if (x != 1)
+                {
+                    this.datos2 = Convert.ToInt32(textBox1.Text);
+                    this.operacion = Convert.ToChar("*");
+                    textBox1.Text = Convert.ToString(Toperacion());
+                    datos1 = rpta;
+                    //this.datos1 = datos2;
+                    //this.datos2 = Convert.ToInt32(null);
+                }
+            else
+                {
+                    this.datos1 = Convert.ToDouble(textBox1.Text);
+                    this.operacion = Convert.ToChar("*");
+                    textBox1.Text = "";
+                    x = 2;
+                }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (operacion != Convert.ToChar("/"))//cambia de una operacion a otra
+            {
+                x = 1;
+            }
+            if (x != 1)
+            {
+                this.datos2 = Convert.ToInt32(textBox1.Text);
+                this.operacion = Convert.ToChar("/");
+                textBox1.Text = Convert.ToString(Toperacion());
+                datos1 = rpta;
+                //this.datos1 = datos2;
+                //this.datos2 = Convert.ToInt32(null);
+            }
+            else
+            {
+                this.datos1 = Convert.ToInt32(textBox1.Text);
+                this.operacion = Convert.ToChar("/");
+                textBox1.Text = "";
+                x = 2;
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            datos1 = 0;
+            datos2 = 0;
+            rpta = 0;
+            Close();
         }
     }
 }
--- Calculadora/Form2.cs
+++ Calculadora/Form2.cs
@@ -11,12 +11,31 @@ namespace Calculadora
 {
     public partial class Form2 : Form
     {
+
         public Form2()
         {
             InitializeComponent();
         }
 
         private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (listBox1.GetItemText(1)== "CELSIUS" && listBox1.SelectedValue == "KELVIN")
+            {
+                textBox3.Text = Convert.ToString(Convert.ToInt32(textBox1.Text) + 273);
+            }
+            if (listBox1.GetItemText(0) == "KELVIN" && listBox2.GetItemText(1) == "CELSIUS")
+            {
+                textBox3.Text = Convert.ToString(Convert.ToInt32(textBox1.Text) - 273);
+            }
+            //CELSIUS KELVIN FAHERNHEIT    (listBox1.SelectedItem == "KELVIN" && listBox2.SelectedItem == "CELSIUS") || 
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
         {
 
         }
